30f26f414dfc56706429d62e303d0bd074ff9d20
chiyanglin <chiyanglin@gmail.com>
2025-03-25 20:25:39 +0800

    Create ex2-2.py

---
+++ ex2-2.py
@@ -0,0 +1,11 @@
+# this is my first program
+
+'''
+---------------------------------
+this is hello python program 
+---------------------------------
+'''
+
+print("hello") # this is my first program
+
+# this is my first program
